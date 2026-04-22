2c0f8c83ce6127ff4b4a152abb81fb7156bbf2cd
thewellington <thewellington@gmail.com>
2026-04-21 17:18:12 -0700

    let figure out mac app store another day

--- nix-modules/homebrew.nix
+++ nix-modules/homebrew.nix
@@ -34,8 +34,8 @@
       #"warp"
     ];
     masApps = {
-      "1Password for Safari" = 1569813296;
-      "Discovery - DNS-SD Browser" = 1381004916;
+      # "1Password for Safari" = 1569813296;
+      # "Discovery - DNS-SD Browser" = 1381004916;
     };
     onActivation.cleanup = "zap"; # none, uninstall, zap
     onActivation.autoUpdate = true;
