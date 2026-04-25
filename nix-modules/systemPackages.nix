d3ebddea26b72c16aa83ae7a44ee069af3fb1947
thewellington <thewellington@gmail.com>
2026-04-25 11:50:42 -0700

    cleaning up

--- nix-modules/systemPackages.nix
+++ nix-modules/systemPackages.nix
@@ -2,29 +2,28 @@
 { config, lib, pkgs, ...}: {
 
   environment.systemPackages = with pkgs; [
-    vim
-    mkalias
-    #
+
     # gui apps
     # arq # does not work yet 2026-04
     bartender
     # bbedit #dies not work yet 2026-04-12
     brave
     discord
-    
     iterm2
-    mas
     zed-editor
     zoom-us
-    #
-    # cli apps
+
+ 		# cli apps
     awscli2
     azure-cli
     cowsay
-    gpp
-    nmap
+    gpp # resume
+    mas # required by home brew to install mas items
+    mkalias
     netlify-cli
-    pandoc
+    nmap
+    pandoc # resume
+    vim #required for nix
 
 
   ];
